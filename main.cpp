e58dcbbcc7fde24413a09ee5584226eec75f3c6e
UsmanTung <usman-tung@hotmail.com>
2025-11-06 02:29:20 -0800

    Implemented Deck

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <array>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,6 +16,8 @@ class Card {
 public:
     string rank;
     Suit suit;
+
+    Card() : suit(Hearts), rank("2") {}
 
     Card(Suit suit, string rank): suit(suit), rank(rank) { }
 
@@ -30,9 +38,44 @@ public:
     }
 };
 
+class Deck {
+private:
+    std::array<Card, 52> cards;
+    int index;
+public:
+    Deck() {
+        int count = 0;
+        for (int i=0; i<4; i++){
+            for(int j=0; j<13; j++){
+                this->cards[count++] = Card((Suit)i, RANKVALUES[j]);
+            }
+        }
+    }
+    
+    void shuffleCards() {
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::shuffle(this->cards.begin(), this->cards.end(), std::default_random_engine(seed));
+        this->index = 0;
+    }
+
+    const Card& operator[](int i) const{
+        return this->cards[i];
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Deck& myDeck){
+        for (int i=0; i < 52; i++){
+            os << myDeck[i] << endl;
+        }
+        return os;
+    }
+
+};
+
 
 int main(){
-    Card myCard = Card(Suit(0),"J");
-    std::cout << myCard;
+    Deck cards = Deck();
+    std::cout << cards << endl;
+    cards.shuffleCards();
+    std::cout << cards;
     return 0;
 }
